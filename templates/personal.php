ae25e47554ccc1bd4737aeac302fc7665f6d5595
root <root@vmi30433.contabo.net>
2017-12-14 15:36:11 +0100

    personal settings WIP

---
+++ templates/personal.php
@@ -0,0 +1,11 @@
+<div class="section">
+  <h2>Folder Player</h2>
+
+  <textarea id="folderplayer_parameters" name="folderplayer_parameters" type="textarea" class="textarea">
+    <?php if ($_['folderplayer_parameters']) : ?>
+      <?php print $_['folderplayer_parameters'] ?>
+    <?php endif; ?>
+  </textarea>
+  
+  <br><?php var_dump($_); ?>
+</div>
