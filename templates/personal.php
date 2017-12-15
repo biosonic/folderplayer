f8ab0d25a5d78ce9dfe7c57761db6468822251ca
root <root@vmi30433.contabo.net>
2017-12-15 14:14:55 +0100

    settings wip...

--- templates/personal.php
+++ templates/personal.php
@@ -1,11 +1,20 @@
+<style>
+  #folderplayer_parameters {
+    max-width: 100%;
+  }
+
+</style>
+
 <div class="section">
   <h2>Folder Player</h2>
 
-  <textarea id="folderplayer_parameters" name="folderplayer_parameters" type="textarea" class="textarea">
+  <textarea rows="10" cols="50" id="folderplayer_parameters" name="folderplayer_parameters" type="textarea" class="textarea">
     <?php if ($_['folderplayer_parameters']) : ?>
       <?php print $_['folderplayer_parameters'] ?>
     <?php endif; ?>
   </textarea>
-  
+
+  <button>
+
   <br><?php var_dump($_); ?>
 </div>
